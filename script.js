b95863b4c28c434ba8e48b6a3da362fa83c735e6
Cheniqua Breaux <cbreaux2@mkc02dq300md6r.humad.com>
2023-11-03 18:06:37 -0400

    first commit and initial stylings

---
+++ script.js
@@ -0,0 +1,10 @@
+// 1. page layout
+// 2. styling (crop header image some more)(color scheme)(button size)(font size) (background color for game container)
+// 3. radio button and submit button
+// 4. maybe highlight or pop out the area that has been chosen (maybe just on hover)
+// 5. computer needs to generate rock paper or scissors 
+// 6. alert saying if player has won, lost or tied (when game is lost say paper covers rock, rock smashes scissors, scissors cut paper)
+// 7. display players choice and computer's choice  (maybe two box areas with pics. highlight computer's choice
+//     after player has guessed, as well as player's guess. )
+// 8. only one round involved and then game is refreshed
+
