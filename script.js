6d76b154a1ca278130af8dde0a428d582d2137c4
Cheniqua Breaux <cbreaux2@mkc02dq300md6r.humad.com>
2023-11-07 17:12:29 -0500

    added more styling

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 // 1. page layout
 // 2. styling (crop header image some more)(color scheme)(button size)(font size) (background color for game container)
-// 3. radio button and submit button
+// 3.  button for player images and submit button
 // 4. maybe highlight or pop out the area that has been chosen (maybe just on hover)
 // 5. computer needs to generate rock paper or scissors 
-// 6. alert saying if player has won, lost or tied (when game is lost say paper covers rock, rock smashes scissors, scissors cut paper)
-// 7. display players choice and computer's choice  (maybe two box areas with pics. highlight computer's choice
-//     after player has guessed, as well as player's guess. )
-// 8. only one round involved and then game is refreshed
+let computer = ["Rock", "Paper", "Scissors"]
+let i = Math.floor(Math.random() * computer.length)
+console.log(computer[i])
 
+/* 6. Player makes a choice. get the value of the choice to compare to the computer "String" */
+
+    
+
+
+
+
+// 7. alert saying if player has won, lost or tied (when game is lost say paper covers rock, rock smashes scissors, scissors cut paper)
+// 8. display players choice and computer's choice  (maybe two box areas with pics. highlight computer's choice
+//     after player has guessed, as well as player's guess. )
+// 9. only one round involved and then game is refreshed
+
+
+
+//set an empty array for player choice 
+
+
+/// dont use the array, give each button an id and call it that way. Set it to a variable. let that variable equal a string to compare to the array string
+
+
+// function results(){
+//     document.getElementById("submit").innerHTML = "You Win!!"
+    // document.getElementById("submit").innerHTML = "You Lose"
+    // document.getElementById("submit").innerHTML = "You Tied"
+// }
