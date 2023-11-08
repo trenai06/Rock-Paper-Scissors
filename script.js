61a76312a681f7bcd1497e36187173b3cbd75d50
Cheniqua Breaux <cbreaux2@mkc02dq300md6r.humad.com>
2023-11-08 17:38:47 -0500

    finished styling added most of the functionality

--- script.js
+++ script.js
@@ -3,32 +3,69 @@
 // 3.  button for player images and submit button
 // 4. maybe highlight or pop out the area that has been chosen (maybe just on hover)
 // 5. computer needs to generate rock paper or scissors 
-let computer = ["Rock", "Paper", "Scissors"]
-let i = Math.floor(Math.random() * computer.length)
-console.log(computer[i])
-
+    let computer = ["Rock", "Paper", "Scissors"]
+    let i = Math.floor(Math.random() * computer.length)
+    computerAnswer = computer[i]
+    console.log(computerAnswer)
 /* 6. Player makes a choice. get the value of the choice to compare to the computer "String" */
 
-    
+    let button 
+    function buttonCheck(buttonvalue){
+        button = buttonvalue
+        console.log(button)
+    }
 
+    computerScore = ["3", "2", "1"]
+    playerScore = ["3", "2", "1"]
 
+    // 7. alert saying if player has won or lost  (when game is lost say paper covers rock, rock smashes scissors, scissors cut paper)
 
+    function gameResults(){
+        
+        if (button === computerAnswer){
+            document.getElementById("submit").innerHTML = (`It's a tie!!`)
+        } else if (button === "Rock" && computerAnswer === "Paper"){
+            document.getElementById("submit").innerHTML = (`Sorry, Paper covers rock!`)
+            document.getElementById("computer-score").innerHTML = computerScore.pop()
+        } else if (button === "Paper" && computerAnswer === "Scissors"){
+            document.getElementById("submit").innerHTML = (`Sorry, Scissors cut paper!`)
+            document.getElementById("computer-score").innerHTML = computerScore.pop()
+        } else if (button === "Scissors" && computerAnswer === `Rock`){
+            document.getElementById("submit").innerHTML = (`Sorry, Rock smashes scissors!`)
+            document.getElementById("computer-score").innerHTML = computerScore.pop()
+        } else if(button === "Rock" && computerAnswer === "Scissors"){
+            document.getElementById("submit").innerHTML = (`Yes! Rock smashes scissors!`)
+            document.getElementById("player-score").innerHTML = playerScore.pop()
+        } else if(button === "Scissors" && computerAnswer === "Paper"){
+            document.getElementById("submit").innerHTML = (`Yes! Scissors cut paper!`)
+            document.getElementById("player-score").innerHTML = playerScore.pop()
+        } else if(button === "Paper" && computerAnswer === "Rock"){
+            document.getElementById("submit").innerHTML = (`Yes! Paper covers rock!`)
+            document.getElementById("player-score").innerHTML = playerScore.pop()
+        } else{
+            document.getElementById("submit").innerHTML = (`Please, Click a button!`)
+        }
+        }
 
-// 7. alert saying if player has won, lost or tied (when game is lost say paper covers rock, rock smashes scissors, scissors cut paper)
-// 8. display players choice and computer's choice  (maybe two box areas with pics. highlight computer's choice
+        // 8. only three rounds and then game is refreshed
+    function gameWinOrLose(){
+        if(computerScore == computerScore.length){
+            alert(`Game Over! Better Luck Next Time!`)
+            window.location.reload()
+        } else if(playerScore == playerScore.length){
+            alert(`You Win!!!! Would You Like To Try Again?`)
+            window.location.reload()
+        }
+    }
+
+    //9. a way to reset the game after a choice without refreshing score 
+
+   
+
+// 10. display players choice and computer's choice  (maybe two box areas with pics. highlight computer's choice
 //     after player has guessed, as well as player's guess. )
-// 9. only one round involved and then game is refreshed
 
 
 
-//set an empty array for player choice 
 
 
-/// dont use the array, give each button an id and call it that way. Set it to a variable. let that variable equal a string to compare to the array string
-
-
-// function results(){
-//     document.getElementById("submit").innerHTML = "You Win!!"
-    // document.getElementById("submit").innerHTML = "You Lose"
-    // document.getElementById("submit").innerHTML = "You Tied"
-// }
