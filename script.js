afd04d61e345d0cb5149acd17db399f18652acc2
Cheniqua Breaux <cbreaux2@mkc02dq300md6r.humad.com>
2023-11-08 19:26:50 -0500

    finished styling and functionality

--- script.js
+++ script.js
@@ -15,39 +15,62 @@
         console.log(button)
     }
 
+    function arrayPop(){
+        computer.pop()
+    }
+
     computerScore = ["3", "2", "1"]
     playerScore = ["3", "2", "1"]
 
     // 7. alert saying if player has won or lost  (when game is lost say paper covers rock, rock smashes scissors, scissors cut paper)
-
+    // 8. a way to reset the game after a choice without refreshing score 
     function gameResults(){
         
         if (button === computerAnswer){
-            document.getElementById("submit").innerHTML = (`It's a tie!!`)
+            alert(`It Is A Draw!`)
+            window.location.reload()
         } else if (button === "Rock" && computerAnswer === "Paper"){
-            document.getElementById("submit").innerHTML = (`Sorry, Paper covers rock!`)
+            document.getElementById("submit").innerHTML = (`Paper covers rock! Try Again!`)
             document.getElementById("computer-score").innerHTML = computerScore.pop()
+            computerAnswer = "Scissors"
+            console.log(computerAnswer)
+            
         } else if (button === "Paper" && computerAnswer === "Scissors"){
-            document.getElementById("submit").innerHTML = (`Sorry, Scissors cut paper!`)
+            document.getElementById("submit").innerHTML = (`Scissors cut paper! Try Again!`)
             document.getElementById("computer-score").innerHTML = computerScore.pop()
+            computerAnswer = " Rock"
+            console.log(computerAnswer)
+
         } else if (button === "Scissors" && computerAnswer === `Rock`){
-            document.getElementById("submit").innerHTML = (`Sorry, Rock smashes scissors!`)
+            document.getElementById("submit").innerHTML = (`Rock smashes scissors! Try Again!`)
             document.getElementById("computer-score").innerHTML = computerScore.pop()
+            computerAnswer = "Paper"
+            console.log(computerAnswer)
+
         } else if(button === "Rock" && computerAnswer === "Scissors"){
-            document.getElementById("submit").innerHTML = (`Yes! Rock smashes scissors!`)
+            document.getElementById("submit").innerHTML = (`Yes! Rock smashes scissors! Go Again!`)
             document.getElementById("player-score").innerHTML = playerScore.pop()
+            computerAnswer = "Paper"
+            console.log(computerAnswer)
+
         } else if(button === "Scissors" && computerAnswer === "Paper"){
-            document.getElementById("submit").innerHTML = (`Yes! Scissors cut paper!`)
+            document.getElementById("submit").innerHTML = (`Yes! Scissors cut paper! Go Again!`)
             document.getElementById("player-score").innerHTML = playerScore.pop()
+            computerAnswer = "Rock"
+            console.log(computerAnswer)
+
         } else if(button === "Paper" && computerAnswer === "Rock"){
-            document.getElementById("submit").innerHTML = (`Yes! Paper covers rock!`)
+            document.getElementById("submit").innerHTML = (`Yes! Paper covers rock! Go Again!`)
             document.getElementById("player-score").innerHTML = playerScore.pop()
+            computerAnswer = "Scissors"
+            console.log(computerAnswer)
+
         } else{
             document.getElementById("submit").innerHTML = (`Please, Click a button!`)
         }
         }
 
-        // 8. only three rounds and then game is refreshed
+        // 9. only three rounds and then game is refreshed
     function gameWinOrLose(){
         if(computerScore == computerScore.length){
             alert(`Game Over! Better Luck Next Time!`)
@@ -58,12 +81,26 @@
         }
     }
 
-    //9. a way to reset the game after a choice without refreshing score 
+    function rockSelect(){
+        document.getElementById("rock").style.border = 'solid 10px teal'
+     }
 
-   
+     function paperSelect(){
+        document.getElementById("paper").style.border = 'solid 10px teal'
+     }
+
+     function scissorsSelect(){
+         document.getElementById("scissors").style.border = 'solid 10px teal'
+     } 
+
+
+    function textReset(){
+        document.getElementById("submit").innerHTML = (`...`)
+    }
 
 // 10. display players choice and computer's choice  (maybe two box areas with pics. highlight computer's choice
 //     after player has guessed, as well as player's guess. )
+// Maybe Not!!
 
 
 
