8d55bd2c82ecf54a04f3fc53d921b1b15acd5741
Cheniqua Breaux <cbreaux2@mkc02dq300md6r.humad.com>
2023-11-09 11:57:43 -0500

    Code cleanup

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-// 1. page layout
-// 2. styling (crop header image some more)(color scheme)(button size)(font size) (background color for game container)
-// 3.  button for player images and submit button
-// 4. maybe highlight or pop out the area that has been chosen (maybe just on hover)
-// 5. computer needs to generate rock paper or scissors 
     let computer = ["Rock", "Paper", "Scissors"]
     let i = Math.floor(Math.random() * computer.length)
     computerAnswer = computer[i]
     console.log(computerAnswer)
-/* 6. Player makes a choice. get the value of the choice to compare to the computer "String" */
+
 
     let button 
     function buttonCheck(buttonvalue){
@@ -22,8 +17,7 @@
     computerScore = ["3", "2", "1"]
     playerScore = ["3", "2", "1"]
 
-    // 7. alert saying if player has won or lost  (when game is lost say paper covers rock, rock smashes scissors, scissors cut paper)
-    // 8. a way to reset the game after a choice without refreshing score 
+    
     function gameResults(){
         
         if (button === computerAnswer){
@@ -70,7 +64,6 @@
         }
         }
 
-        // 9. only three rounds and then game is refreshed
     function gameWinOrLose(){
         if(computerScore == computerScore.length){
             alert(`Game Over! Better Luck Next Time!`)
@@ -93,14 +86,11 @@
          document.getElementById("scissors").style.border = 'solid 10px teal'
      } 
 
-
     function textReset(){
         document.getElementById("submit").innerHTML = (`...`)
     }
 
-// 10. display players choice and computer's choice  (maybe two box areas with pics. highlight computer's choice
-//     after player has guessed, as well as player's guess. )
-// Maybe Not!!
+
 
 
 
